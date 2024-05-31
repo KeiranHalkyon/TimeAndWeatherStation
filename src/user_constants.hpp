50584c4e4a0c53c39b6894da95dfc16b7d0e9d92
Mriganka Manna <mrigankamanna3@gmail.com>
2024-05-31 13:44:06 +0530

    update constant variables

--- src/user_constants.hpp
+++ src/user_constants.hpp
@@ -1,13 +1,13 @@
 #ifndef USER_CONSTANTS
 #define USER_CONSTANTS
 
-const char *ssid = "WIFI_SSID",
-        *password = "WIFI_PASSWORD",
-        *remote_host = "www.google.com"; //this is pinged for connectivity test
+const char PROGMEM ssid[] = "WIFI_SSID",
+        password[] = "WIFI_PASSWORD",
+        remote_host[] = "www.google.com"; //this is pinged for connectivity test
 
-const String rdsUrl = "URL",               //URL that points to http endpoint of our lambda function in AWS
-        openWeatherUrl = "URL",                                                //Initial permanent part of the Open Weather Map API URL
-        rdsApiKey = "API_KEY",                                                 //API key for our lambda API in AWS
-        openWeatherApiKey = "API_KEY";                                                 //API key for Open Weather Map
+const char PROGMEM rdsUrl[] = R"deli(https://AWS_URL?tempBMP=%0.2f&pressBMP=%0.2f&tempAHT=%0.2f&humAHT=%0.2f)deli",   //URL that points to http endpoint of our lambda function in AWS with parameters
+        openWeatherUrl[] = R"del(https://api.openweathermap.org/data/3.0/onecall?appid=%s&lat=%s&lon=%s&units=metric&exclude=minutely)del",   // OpenWeatherMap 3.0 API URL with parameters
+        rdsApiKey[] = "API_KEY",                                       //API key for our lambda API in AWS
+        openWeatherApiKey[] = "API_KEY";                                               //API key for Open Weather Map
 
 #endif
